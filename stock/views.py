6cb4a884887a2dff7ce9ea9d85fe605b922e5389
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-02-03 18:24:02 +0300

    serializers

--- stock/views.py
+++ stock/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from .models import Category,Brand,Product,Firm,Purchases,Sales
-from .serializers import CategorySerializer
+from .serializers import CategorySerializer,CategoryProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # Create your views here.
 class CategoryView(viewsets.ModelViewSet):
     queryset=Category.objects.all()
     serializer_class=CategorySerializer
+    filter_backends=[filters.SearchFilter,DjangoFilterBackend]
+    search_fields=['name']
+    filterset_fields=["name"]
+    
+    def get_serializer_class(self):
+        if self.request.query_params.get("name"):
+            return CategoryProductSerializer
+        return super().get_serializer_class()
+
