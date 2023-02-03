4ece92559feb357bbba8a3a5d5cc16ee6efaf90f
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-02-03 20:17:13 +0300

    done

--- stock/views.py
+++ stock/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets,filters
 from .models import Category,Brand,Product,Firm,Purchases,Sales
 from .serializers import CategorySerializer,CategoryProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.permissions import DjangoModelPermissions
 
 # Create your views here.
 class CategoryView(viewsets.ModelViewSet):
@@ -12,7 +12,7 @@ class CategoryView(viewsets.ModelViewSet):
     filter_backends=[filters.SearchFilter,DjangoFilterBackend]
     search_fields=['name']
     filterset_fields=["name"]
-    
+    permission_classes=[DjangoModelPermissions]
     def get_serializer_class(self):
         if self.request.query_params.get("name"):
             return CategoryProductSerializer
